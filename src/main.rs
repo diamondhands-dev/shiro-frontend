e5acea5a7ba36ae4b055ecc3ec70cd7ab83603b4
Masaki Muranaka <monaka@monami-ya.com>
2023-01-29 15:33:39 +0900

    Add hash prefixes.
    This should be reverted.
    
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,15 @@ mod components;
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route {
-    #[at("/")]
+    #[at("#/")]
     BalanceTabRoute,
-    #[at("/bitcoin")]
+    #[at("#/bitcoin")]
     BitcoinPageRoute,
-    #[at("/issue")]
+    #[at("#/issue")]
     IssueAssetPageRoute,
-    #[at("/mnemonic")]
+    #[at("#/mnemonic")]
     MnemonicPageRoute,
-    #[at("/utxos")]
+    #[at("#/utxos")]
     UtxosPageRoute,
 }
 
@@ -83,11 +83,11 @@ fn app() -> Html {
     html! {
         <div>
             <div class="navbar navbar-default">
-                <a class="navbar-brand" href="/">{"Shiro-wallet"}</a>
-                <a class="navbar-link" href="bitcoin">{"bitcoin"}</a>
-                <a class="navbar-link" href="issue">{"issue"}</a>
-                <a class="navbar-link" href="mnemonic">{"mnemonic"}</a>
-                <a cless="navbar-link" href="utxos">{"utxos"}</a>
+                <a class="navbar-brand" href="#/">{"Shiro-wallet"}</a>
+                <a class="navbar-link" href="#/bitcoin">{"bitcoin"}</a>
+                <a class="navbar-link" href="#/issue">{"issue"}</a>
+                <a class="navbar-link" href="#/mnemonic">{"mnemonic"}</a>
+                <a cless="navbar-link" href="#/utxos">{"utxos"}</a>
             </div>
             <BrowserRouter>
                 <Switch<Route> render={Switch::render(switch)} />
